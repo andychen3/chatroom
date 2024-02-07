8e9dfd6f72ff430e0c9734b774ac19954074c566
Andy <andychen.105@gmail.com>
2024-02-07 17:48:16 -0500

    Created server viewsets with filtering capabilities.

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -24,12 +24,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    "account",
+    "server",
+    # Pre-installed apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Installed apps
+    "drf_spectacular",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +121,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+}
---
+++ server/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Server
+from .serializer import ServerSerializer
+from rest_framework.response import Response
+
+
+class ServerListViewSet(viewsets.ViewSet):
+
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        serializer = ServerSerializer(self.queryset, many=True)
+        return Response(serializer.data)
