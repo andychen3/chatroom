1b22eba79d85f10e72e182a0dcd6ce6b0a87bb9c
Andy <andychen.105@gmail.com>
2024-02-09 21:19:53 -0500

    Created functionality to add and delete image once category gets removed.

--- server/models.py
+++ server/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+
+
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
 
 class Server(models.Model):
@@ -22,7 +44,7 @@ class Server(models.Model):
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}-{self.id}"
 
 
 class Channel(models.Model):
